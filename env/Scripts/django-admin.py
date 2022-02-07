d5314faa076ceabdbf104df2350bef0cb2237005
Jo <joteq7@gmail.com>
2022-02-07 13:27:22 +0000

    my first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Jo\OneDrive\Desktop\Django-Tutorials\Udemy\Rathan\carzone-project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
